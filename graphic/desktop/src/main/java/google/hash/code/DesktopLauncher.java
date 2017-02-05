5bdd361670e50ba217877611b881c2a499f99db2
juju <girardot.jul@gmail.com>
2017-02-05 17:33:09 +0100

    feat(graphic): manage multiple teams

--- graphic/desktop/src/main/java/google/hash/code/DesktopLauncher.java
+++ graphic/desktop/src/main/java/google/hash/code/DesktopLauncher.java
@@ -2,6 +2,7 @@ package google.hash.code;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.google.common.collect.Lists;
 import google.hash.code.input.InputReader;
 import google.hash.code.model.Position;
 import google.hash.code.model.World;
@@ -12,6 +13,7 @@ import org.slf4j.Logger;
 
 import java.io.FileNotFoundException;
 import java.net.URISyntaxException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -30,25 +32,40 @@ public class DesktopLauncher {
 
         final String[] fileNames = {"simple.in"};
 //        final String[] fileNames = {"simple.in", "busy_day.in", "redundancy.in", "mother_of_all_warehouses.in"};
+        final List<TeamScore> teams = Lists.newArrayList(new TeamScore("BIBO"), new TeamScore("Test"));
 
         for (String fileName : fileNames) {
             LOGGER.info("File " + fileName + " loading");
             World world = inputReader.parse("/" + fileName);
-            final List<ScoreDrone> drones = outputFileReader.parse("/" + fileName.replace(".in", ".out"), world);
 
-            LOGGER.info("Score Processing, Actions computing");
-            LOGGER.info("Score => " + scoreProcessor.computeScore(world, drones));
+            final List<ScoreDrone> allTeamDrones = new ArrayList<>();
+//                final List<ScoreDrone> allTeamDrones = outputFileReader.parse("/" + fileName.replace(".in", ".out"), world);
+            for (TeamScore team : teams) {
+                String outFilePath = "/out/" + team.getName() + "/" + fileName.replace(".in", "");
+                if (teamFileExist(outFilePath)) {
+                    final List<ScoreDrone> drones = outputFileReader.parse(outFilePath, world);
+                    LOGGER.info("Score Processing, Actions computing");
+                    LOGGER.info("Score => {} ", scoreProcessor.computeScore(world, drones));
+                    allTeamDrones.addAll(drones);
+                } else {
+                    LOGGER.info("Team {} does have the file {}", team.getName(), outFilePath);
+                }
+            }
 
             List<Position> warehouses = world.warehouses.stream()
                     .map(warehouse -> warehouse.position)
                     .collect(Collectors.toList());
 
             new LwjglApplication(
-                    new MyGdxGame(warehouses, drones, world.orders, world.columns, world.rows),
+                    new MyGdxGame(warehouses, allTeamDrones, world.orders, world.columns, world.rows),
                     buildLwjglApplicationConfiguration()
             );
         }
 
+    }
+
+    private static boolean teamFileExist(String filePath) {
+        return InputReader.class.getResource(filePath) != null;
     }
 
     private static LwjglApplicationConfiguration buildLwjglApplicationConfiguration() {
